0cd57f58a9e1ffc88b0a9692f302c6c4d3505b83
Somnier Facundo <145161370+FacundoSomnier@users.noreply.github.com>
2023-11-08 14:51:21 -0300

    arreglo de plantillas nuevas y backend

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 @app.route("/home/<tipo>.html")
 def tipo(tipo):
---
+++ test.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def activar_menu(tipo):
+    global clase_1, clase_2, clase_3, clase_4, clase_5
+    if tipo == "ventas":
+        clase_1 = None
+        clase_2 = 'active'
+        clase_3 = None
+        clase_4 = None
+        clase_5 = None
+    elif tipo == "alquileres":
+        clase_1 = None
+        clase_2 = None
+        clase_3 = 'active'
+        clase_4 = None
+        clase_5 = None
+    elif tipo == "zonas":
+        clase_1 = None
+        clase_2 = None
+        clase_3 = None
+        clase_4 = 'active'
+        clase_5 = None
+    elif tipo == "contacto":
+        clase_1 = None
+        clase_2 = None
+        clase_3 = None
+        clase_4 = None
+        clase_5 = 'active'
+@app.route("/")
+def home():
+    return render_template("index.html", clase1='active')
+
+
+@app.route("/<tipo>")
+def tipo(tipo):
+    activar_menu(tipo)
+    return render_template("plantillaV_A.html", tipo=tipo, clase1=clase_1, clase2=clase_2, clase3=clase_3, clase4=clase_4, clase5=clase_5)
+
+@app.route("/<tipo>/<clase>")
+def plantilla(tipo, clase):
+    activar_menu(tipo)
+    return render_template("plantillaProp.html", tipo=tipo, clase1=clase_1, clase2=clase_2, clase3=clase_3, clase4=clase_4, clase5=clase_5)
+
+@app.route("/contacto")
+def contacto():
+    activar_menu("contacto")
+    return render_template("contact.html", tipo=tipo, clase1=clase_1, clase2=clase_2, clase3=clase_3, clase4=clase_4, clase5=clase_5)
+
+@app.route("/zonas")
+def zonas():
+    activar_menu("zonas")
+    return render_template("plantillaZonas.html", tipo=tipo, clase1=clase_1, clase2=clase_2, clase3=clase_3, clase4=clase_4, clase5=clase_5)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
